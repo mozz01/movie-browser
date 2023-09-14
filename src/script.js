1a7e78668130777161c657d8d92ae316a1b4ae38
Error-Mozz13 <mozz7e6@gmail.com>
2023-09-14 01:48:12 -0700

    Fixed thumbnails fetching issue

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const API_LINK = 'https://api.themoviedb.org/3/movie/550?api_key=fef390b999cfcff68feb5b573745f88a'
+const API_LINK = 'https://api.themoviedb.org/3/search/movie?query=smile&include_adult=false&language=en-US&page=1'
 const IMAGE_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?query='
 
@@ -6,12 +6,20 @@ const main = document.getElementById("section")
 const form = document.getElementById("form")
 const searchBox = document.getElementById("query")
 
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWYzOTBiOTk5Y2ZjZmY2OGZlYjViNTczNzQ1Zjg4YSIsInN1YiI6IjY1MDI4Y2I3NmEyMjI3MDBhYmE5ODI1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rH64s0cBvk3L_ZfJW7p0BcmunY3g8WjImGv41N6tPDs'
+    }
+  };
+
 getMovies(API_LINK);
 
 function getMovies(url)
 {
-    fetch(url)
-    .then(res => res.json())
+    fetch(url, options)
+    .then(response => response.json())
     .then(function(data)
     {
         console.log(data.results);
@@ -20,7 +28,6 @@ function getMovies(url)
         {
             const rowDiv = document.createElement('div');
             rowDiv.setAttribute('class', 'row');
-            rowDiv.setAttribute('style', 'padding: 10px');
 
             const columnDiv = document.createElement('div');
             columnDiv.setAttribute('class', 'column');
@@ -31,25 +38,33 @@ function getMovies(url)
             const imageContainerDiv = document.createElement('div');
             imageContainerDiv.setAttribute('class', 'image-container');
 
-            const imageDiv = document.createElement('div');
-            imageDiv.setAttribute('class', 'thumbnail');
-            imageDiv.setAttribute('draggable', 'false');
-            imageDiv.setAttribute('id', 'movie_image');
+            const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('draggable', 'false');
+            image.setAttribute('id', 'movie_image');
 
             const titleDiv = document.createElement('h3');
             titleDiv.setAttribute('id', 'movie_title');
 
-            titleDiv.innerText = "${element.title}";
-            imageDiv.src = IMAGE_PATH + element.poster_path;
+            titleDiv.innerText = `${element.title}`;
+            if(element.poster_path)
+            {
+                image.setAttribute('src', IMAGE_PATH + element.poster_path);
+            }
+            else
+            {
+                image.setAttribute('src', 'images/not_found.png');
+            }
 
-            imageContainerDiv.appendChild(imageDiv);
+            imageContainerDiv.appendChild(image);
             cardDiv.append(imageContainerDiv, titleDiv);
             columnDiv.appendChild(cardDiv);
             rowDiv.appendChild(columnDiv);
             
             main.appendChild(rowDiv);
-        });
-    });
+        })
+    })
+    .catch(err => console.error(err));
 }
 
 form.addEventListener("submit", (e) => 
