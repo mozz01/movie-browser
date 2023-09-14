f83b85b47e849226255c987e951b9d581988ea45
Error-Mozz13 <mozz7e6@gmail.com>
2023-09-13 22:56:29 -0700

    Added an image and title container for data results and completed the event listener function for the search box

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ getMovies(API_LINK);
 function getMovies(url)
 {
     fetch(url)
-    .then(res => res.jason())
+    .then(res => res.json())
     .then(function(data)
     {
         console.log(data.results);
@@ -19,16 +19,50 @@ function getMovies(url)
         data.results.forEach(element => 
         {
             const rowDiv = document.createElement('div');
+            rowDiv.setAttribute('class', 'row');
+            rowDiv.setAttribute('style', 'padding: 10px');
+
             const columnDiv = document.createElement('div');
+            columnDiv.setAttribute('class', 'column');
+
             const cardDiv = document.createElement('div');
-            const imageDiv = document.createElement('div');
-            const titleDiv = document.createElement('h3');
+            cardDiv.setAttribute('class', 'card');
+
             const imageContainerDiv = document.createElement('div');
+            imageContainerDiv.setAttribute('class', 'image-container');
+
+            const imageDiv = document.createElement('div');
+            imageDiv.setAttribute('class', 'thumbnail');
+            imageDiv.setAttribute('draggable', 'false');
+            imageDiv.setAttribute('id', 'movie_image');
+
+            const titleDiv = document.createElement('h3');
+            titleDiv.setAttribute('id', 'movie_title');
 
             titleDiv.innerText = "${element.title}";
             imageDiv.src = IMAGE_PATH + element.poster_path;
 
+            imageContainerDiv.appendChild(imageDiv);
+            cardDiv.append(imageContainerDiv, titleDiv);
+            columnDiv.appendChild(cardDiv);
+            rowDiv.appendChild(columnDiv);
+            
+            main.appendChild(rowDiv);
         });
-
     });
 }
+
+form.addEventListener("submit", (e) => 
+{
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchPhrase = searchBox.value;
+
+    if(searchPhrase)
+    {
+        getMovies(SEARCH_API + searchPhrase);
+        searchBox.value = "";
+    }
+
+});
